9ee000bd0223388bfc26fb3fcc673ce60662e88b
Michael Berelejis <michaelbere@gmail.com>
2019-11-24 19:37:23 +0200

    first skeletom implementation
    first game skeleton is implemented.
    
    Icludes:
     * Game movement with arrows
     * Snake growth is simulated with arrow keys
     * Board flips only upon arrival to edges (flips to the right direction)
    
    Missing:
     * Food generation and behaviour
     * mid board flip (enter through a tunnel or somthing like that)
     * self-collision death (Colliders?)
    
    Bugs:
     * something in the end of the fliping is gliching - need to investigate

---
+++ Assets/Scripts/BoardFlippingScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BoardFlippingScript : MonoBehaviour
+{
+    public Transform snakeHead;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void flip(SnakeHeadScript.Flip flip, float duration)
+    {
+        switch (flip)
+        {
+            case SnakeHeadScript.Flip.LEFT:
+                transform.DOLocalRotate(Vector3.back * 180, duration, mode:RotateMode.LocalAxisAdd);
+                break;
+            case SnakeHeadScript.Flip.RIGHT:
+                transform.DOLocalRotate(Vector3.forward * 180, duration, mode: RotateMode.LocalAxisAdd);
+                break;
+            case SnakeHeadScript.Flip.UP:
+                transform.DOLocalRotate(Vector3.left * 180, duration, mode:RotateMode.LocalAxisAdd);
+                break;
+            case SnakeHeadScript.Flip.DOWN:
+                transform.DOLocalRotate(Vector3.right * 180, duration, mode:RotateMode.LocalAxisAdd);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/SnakeBodyScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeBodyScript : MonoBehaviour
+{
+
+    public Transform prevBodyPart;
+    public Transform nextBodyPart;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected bool isTail()
+    {
+        return nextBodyPart == null;
+    }
+
+    public void move(Vector3 newPosition, bool didEat)
+    {
+        Vector3 oldPosition = transform.localPosition;
+        transform.localPosition = newPosition;
+        if (isTail())
+        {
+            if(didEat){
+                GameObject newPart = Instantiate(Resources.Load("Body")) as GameObject;
+                newPart.transform.SetParent(transform.parent);
+                SnakeBodyScript newPartScript = newPart.GetComponent<SnakeBodyScript>();
+                newPartScript.prevBodyPart = transform;
+                GetComponent<SnakeBodyScript>().nextBodyPart = newPart.transform;
+                nextBodyPart.GetComponent<SnakeBodyScript>().move(oldPosition, false);
+            }
+        }
+        else
+        {
+            nextBodyPart.GetComponent<SnakeBodyScript>().move(oldPosition, didEat);
+        }
+    }
+}
