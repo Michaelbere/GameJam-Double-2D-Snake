cb60c0194d938fb0d613037ead3c6ef04986012e
ErezHecht <erez.hecht@gmail.com>
2019-12-09 13:33:35 +0200

    dryaircontroller

---
+++ Assets/Scripts/DryAirController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DryAirController : MonoBehaviour
+{
+    private BarController _dryBar;
+    private BarController _airBar;
+    private bool _flipping = false;
+    private bool _upSide = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetControllers();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_flipping)
+        {
+            _dryBar.UpdateBar(_upSide);
+            _airBar.UpdateBar(!_upSide); 
+        }
+    }
+
+
+    private void SetControllers()
+    {
+        _dryBar = GameObject.Find("DryBar").GetComponent<BarController>();
+        _airBar = GameObject.Find("AirBar").GetComponent<BarController>();
+    }
+    
+    private void Flip()
+    {
+        _flipping = true;
+        _upSide = !_upSide;
+    }
+
+    public void ContinueRunning()
+    {
+        _flipping = false;
+    }
+}
